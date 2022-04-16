b0e8d66c66d30d78a0a2f3858938d56c07dc1b56
hrz07 <hridoy.developer@yahoo.com>
2022-04-16 23:56:24 +0600

    why me section done

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Home.css'
+import MapPhoto from '../../photos/mapping.jpg'
 
 const Home = () => {
     return (
@@ -9,6 +10,23 @@ const Home = () => {
                     Explore Amazing Places With Me
                 </h1>
                 <button className='aboutBtn'>About Me</button>
+            </div>
+            <div className='whyMe'>
+                <div className='whyMeText'>
+                    <h1>Why Me ?</h1>
+                    <p>`Well! I have more than 6 years tour guiding experience. I know all tourist places and thousand of history of Bangladesh. I'll explain your all during tour time. I'll care you to being safe`</p>
+                </div>
+                <div>
+                    <img src={MapPhoto}  />
+                </div>
+            </div>
+            <div className='spotsContainer'>
+                <h3>Top Places I Guide</h3>
+                <div className='spots'>
+                    {
+                        
+                    }
+                </div>
             </div>
         </div>
     );
