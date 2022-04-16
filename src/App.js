dfacd1080c00cb03cf1e183cf58bdeef27131e1e
hrz07 <hridoy.developer@yahoo.com>
2022-04-17 01:15:58 +0600

    not found page added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './Components/Header/Header';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Components/Home/Home';
+import Notfound from './Components/NotFound/NotFound';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>} ></Route>
+        <Route path='*' element={<Notfound></Notfound>} ></Route>
       </Routes>
     </div>
   );
---
+++ src/Components/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './NotFound.css'
+
+const Notfound = () => {
+    return (
+        <div className='notFoundContainer'>
+            <h1>404`</h1>
+            <p>Sorry... This page is not available</p>
+        </div>
+    );
+}
+
+export default Notfound;
